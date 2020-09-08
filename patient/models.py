0ff9a88b20fea81691457ebc7668440c40ee308b
pallavitri1 <51754054+pallavitri1@users.noreply.github.com>
2020-09-08 20:20:11 +0530

    Add files via upload
    Changed django.contrib.postgres.fields to django.db.models as django.contrib.postgres.fields was going to be deprecated in django 4

--- patient/models.py
+++ patient/models.py
@@ -1,6 +1,6 @@
 import time
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.contrib.auth.models import User
 from village.models import Village
 from mandal.models import Mandal
@@ -24,18 +24,14 @@ class Patient (models.Model):
     weight = models.DecimalField(default = 0.0,decimal_places = 2,max_digits = 4,blank=True)
     height = models.DecimalField(default = 0.0,decimal_places = 2,max_digits=5,blank = True)
     bloodgroup = models.CharField(default=None, max_length=4)
-    patient_status = models.CharField(default="Closed", max_length=50) #Critical/Moderate/Mild
-    maritalstatus = models.CharField(default=None, max_length=15,null = True) # single/married/separated/divorced/widowed
     PVTG = models.CharField(default=None, max_length=5)
-    kidneystatus = models.CharField(max_length=50,blank = True) # good/abnormal
-    deworming = models.BooleanField(default=False)
     #Foreign Keys
     mandal = models.ForeignKey(Mandal,on_delete=models.CASCADE,default = None,null = True)
     phc = models.ForeignKey(PHC,on_delete=models.CASCADE,default = None,null = True)
     villagesec = models.ForeignKey(Village_sec,on_delete=models.CASCADE,default = None,null = True)
     village = models.ForeignKey(Village, on_delete=models.CASCADE,default = None,null = True)
-    type_data = models.CharField(max_length=50,default = "Development")
- 
+    # single/married/separated/divorced/widowed
+    maritalstatus = models.CharField(default=None, max_length=15,null = True)
 
     #Basic Vitals
     BasicVitals = JSONField(null = True)
@@ -65,41 +61,28 @@ class Patient (models.Model):
     # }
     
     report = JSONField(null = True)
-    #Free For ALL
+    #Free For ALl
+    deworming = models.BooleanField(default=False)
+    patient_status = models.CharField(default="Closed", max_length=50) #Critical/Moderate/Mild
     habits = JSONField(null = True)
-    #FORMAT = 
-    #{ 
-    #    smoking : bool,
-    #    drinking : bool
-    #}   
 
     #Pedal Fields
     pedalEdema = models.CharField(max_length=2,blank = True)
     # if above is yes then ask single/bilateral
-    pedal_profile = JSONField(null = True)
+    pedaltype = models.CharField(max_length=50,blank = True)
+    dateoftesting = models.CharField(blank = True, max_length=10)
+    serumCreatinine = models.DecimalField(max_digits=5, decimal_places=1,blank = True)
+    bloodUrea = models.DecimalField(max_digits=5, decimal_places=1,blank = True)
+    uricAcid = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
+    electrolytes_sodium = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
+    electrolytes_potassium = models.DecimalField(
+        max_digits=5, decimal_places=1,blank = True)
+    bun = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
     
-    #FORMAT =
-    #{
-        # pedaltype = models.CharField(max_length=50, blank=True)
-        # dateoftesting = models.CharField(blank=True, max_length=10)
-        # serumCreatinine = models.DecimalField(
-        #     max_digits=5, decimal_places=1, blank=True)
-        # bloodUrea = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-        # uricAcid = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-        # electrolytes_sodium = models.DecimalField(
-        #     max_digits=5, decimal_places=1, blank=True)
-        # electrolytes_potassium = models.DecimalField(
-        #     max_digits=5, decimal_places=1, blank=True)
-        # bun = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-    #}
-     
-    KidneyProfile = JSONField(null = True)
-    #FORMAT = 
-    {
-        # in case abnormal ask following
-        # ailments = models.TextField(max_length=100,blank=True)
-        # dialysis = models.BooleanField(default = False)
-    }
+    kidneystatus = models.CharField(max_length=50,blank = True) # good/abnormal
+    # in case abnormal ask following
+    ailments = models.TextField(max_length=100,blank=True)
+    dialysis = models.BooleanField(default = False)
     doctorreq = models.BooleanField(default = False)
     # if above is yes then ask refer to the following hospital
     hospitalAdmit = models.CharField(max_length=50,blank = True)
@@ -114,18 +97,13 @@ class Patient (models.Model):
     dischargeStatus = models.TextField(max_length=500,blank=True)
     deceased = models.BooleanField(default = False)
     # if above is answered yes
-    DetailsDeath = JSONField(null = True)
-    #FORMAT =
-    {
-        # deathDate = models.CharField(blank = True, max_length=10)
-        # placeOfDeath = models.CharField(max_length=50,blank=True)
-        # causeOfDeath = models.TextField(max_length=300,blank=True)
-    }
-    AnemiaProfile = JSONField(null = True)
-    #What it will look like:
-    #{
-    #    wbc_count = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
-    #    diffrential_count = JSONField(null = True)
-    #    hb = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
-    #    plat_count = models.DecimalField( max_digits=5, decimal_places=2,blank=True)
-    #}
+    deathDate = models.CharField(blank = True, max_length=10)
+    placeOfDeath = models.CharField(max_length=50,blank=True)
+    causeOfDeath = models.TextField(max_length=300,blank=True)
+    type_data = models.CharField(max_length=50,default = "Development")
+
+    #Anemia Fileds
+    wbc_count = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
+    diffrential_count = JSONField(null = True)
+    hb = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
+    plat_count = models.DecimalField( max_digits=5, decimal_places=2,blank=True)
