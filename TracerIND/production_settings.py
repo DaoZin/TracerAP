3992daad66fcf12af23903027c9a40f1ccdd214c
dEvK4n3Ki <kaneki@aogiridevs.tech>
2020-12-02 11:37:55 +0530

    DEBUG ISSUES

--- TracerIND/production_settings.py
+++ TracerIND/production_settings.py
@@ -3,3 +3,8 @@ ALLOWED_HOSTS = ['149.129.138.15','api-tracerind.covidindiataskforce.org']
 username = 'postgres'
 password = 'admin1234'
 SECRET_KEY = 'f-7=$cp7#_8@8s5-pjpxe!)vh=%qowo=ctqay9@za*gk1gc!i='
+SECURE_HSTS_SECONDS = 60
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
