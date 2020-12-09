3b506b1529c902d11b0ce1d4206c0d2f1d152ee8
dEvK4n3Ki <kaneki@aogiridevs.tech>
2020-12-09 14:06:38 +0530

    HSTS and Hosts fixed

--- TracerIND/production_settings.py
+++ TracerIND/production_settings.py
@@ -1,11 +1,5 @@
-DEBUG = False
-ALLOWED_HOSTS = ['149.129.138.15','api-tracerind.covidindiataskforce.org']
+DEBUG = True
+ALLOWED_HOSTS = ['147.139.38.234','api.tracerap.co.in']
 username = 'postgres'
 password = 'admin1234'
 SECRET_KEY = 'f-7=$cp7#_8@8s5-pjpxe!)vh=%qowo=ctqay9@za*gk1gc!i='
-SECURE_HSTS_SECONDS = 60
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
