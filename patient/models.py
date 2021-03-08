89d88591dc20693a15456808f6e920d0d70928e1
dEvK4n3Ki <shlok.parida@cbnits.com>
2021-03-09 01:14:46 +0530

    Development Started

--- patient/models.py
+++ patient/models.py
@@ -2,44 +2,33 @@ import time
 from django.db import models
 # from django.contrib.postgres.fields import models.JSONField
 from django.contrib.auth.models import User
-from village.models import Village
-from mandal.models import Mandal
-from PHC.models import PHC
-from village_sec.models import Village_sec
-
+from hospital.models import Hospital
+from doctor.models import Doctor
 
 class Patient (models.Model):
     pkid = models.CharField(max_length=32,
                             primary_key=True
                             )
 # BASIC DETAILS
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     phone = models.CharField(max_length=10,blank = True,null = True)
-    adhaar = models.CharField(max_length=256, blank=True)  #hashedData
+    # adhaar = models.CharField(max_length=256, blank=True)  #hashedData
     relation = models.CharField(max_length=30)  # SonOf/DaughterOf/WifeOf
     gaurdian_name = models.CharField(max_length=50)
     age = models.SmallIntegerField(default=0)
     # M=> Male/F=>Female/NB=>NonBinary
     gender = models.CharField(default='NaN', max_length=3)
     address = models.CharField(max_length=500)
-    weight = models.DecimalField(
-        default=0.0, decimal_places=3, max_digits=6, blank=True)
-    height = models.DecimalField(
-        default=0.0, decimal_places=2, max_digits=5, blank=True)
-    bloodgroup = models.CharField(default=None, max_length=4)
-    PVTG = models.CharField(default=None, max_length=5)
-    # Foreign Keys
-    mandal = models.ForeignKey(
-        Mandal, on_delete=models.CASCADE, default=None, null=True)
-    phc = models.ForeignKey(
-        PHC, on_delete=models.CASCADE, default=None, null=True)
-    villagesec = models.ForeignKey(  
-        Village_sec, on_delete=models.CASCADE, default=None, null=True)
-    village = models.ForeignKey(
-        Village, on_delete=models.CASCADE, default=None, null=True)
-    # single/married/separated/divorced/widowed
-    maritalstatus = models.CharField(default=None, max_length=15, null=True)
+    
+    #Bawsic Data
+    BasicData = models.JSONField(null = True)
+    
+    #FORMAT = 
+    # {
+    # weight = models.DecimalField
+    # height = models.DecimalField
+    # bloodgroup = models.CharField
+    # }
 
     # Basic Vitals
     BasicVitals = models.JSONField(null=True)
@@ -77,47 +66,18 @@ class Patient (models.Model):
     #    drinking : bool
     # }
 
-    # Pedal Fields
-    pedalEdema = models.CharField(max_length=2, blank=True)
-    # if above is yes then ask single/bilateral
-
-    pedal_profile = models.JSONField(null=True)
-
-    # FORMAT =
-    # {
-    # pedaltype = models.CharField(max_length=50, blank=True)
-    # dateoftesting = models.CharField(blank=True, max_length=10)
-    # serumCreatinine = models.DecimalField(
-    #     max_digits=5, decimal_places=1, blank=True)
-    # bloodUrea = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-    # uricAcid = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-    # electrolytes_sodium = models.DecimalField(
-    #     max_digits=5, decimal_places=1, blank=True)
-    # electrolytes_potassium = models.DecimalField(
-    #     max_digits=5, decimal_places=1, blank=True)
-    # bun = models.DecimalField(max_digits=5, decimal_places=1, blank=True)
-    # }
-
-    KidneyProfile = models.JSONField(null=True)
-
-    # FORMAT =
-    {
-        # in case abnormal ask following
-        # ailments = models.TextField(max_length=100,blank=True)
-        # dialysis = models.BooleanField(default = False)
-    }
-
     doctorreq = models.BooleanField(default=False)
-    # if above is yes then ask refer to the following hospital
-    hospitalAdmit = models.CharField(max_length=50, blank=True)
-    opd = models.BooleanField(default=False)
-    dateOfAdmit = models.CharField(blank=True, max_length=10)  # YYYY-MM-DD
-    refered = models.BooleanField(default=False)
-    # case:yes
-    referredto = models.CharField(max_length=50, blank=True)
-    ref_status = models.TextField(max_length=300, blank=True)
-    treatmentDone = models.TextField(max_length=300, blank=True)
-    DischargeDetails = models.JSONField(null = True)
+     # if above is yes then ask refer to the following hospital
+    PatientStatus = models.JSONField(null = True)
+    # hospitalAdmit = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    # opd = models.BooleanField(default=False)
+    # dateOfAdmit = models.CharField(blank=True, max_length=10)  # YYYY-MM-DD
+    # refered = models.BooleanField(default=False)
+    # # case:yes
+    # referredto = models.CharField(max_length=50, blank=True)
+    # ref_status = models.TextField(max_length=300, blank=True)
+    # treatmentDone = models.TextField(max_length=300, blank=True)
+    # DischargeDetails = models.JSONField(null = True)
     #FORMAT = 
     # {
     #     discharged = models.BooleanField()
@@ -133,13 +93,3 @@ class Patient (models.Model):
         # placeOfDeath = models.CharField(max_length=50,blank=True)
         # causeOfDeath = models.TextField(max_length=300,blank=True)
     }
-    AnemiaProfile = models.JSONField(null=True)
-    PatientHealthStatus = models.JSONField(null = True)
-    # What it will look like:
-    # {
-    #    wbc_count = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
-    #    diffrential_count = models.JSONField(null = True)
-    #    hb = models.DecimalField(max_digits=5, decimal_places=2,blank = True)
-    #    plat_count = models.DecimalField( max_digits=5, decimal_places=2,blank=True)
-    # }
-    type_data = models.CharField(max_length=50,blank = True,null = True)
